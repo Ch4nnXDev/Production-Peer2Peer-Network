25d110d6f02e2f6e2642b044f4095f7eeb17c26a
Channa2146@gmail.com <Channa2146@gmail.com>
2025-12-18 14:25:45 +0530

    peerManager Complete

--- PeerManager.js
+++ PeerManager.js
@@ -8,9 +8,25 @@ class PeerManager extends EventEmitter {
 
         this.ConnectionManager.on("peer-connected", (peer) => this.addPeer(peer));
         this.ConnectionManager.on("peer-disconnected", (peer) => this.removePeer(peer));
-        
-        
 
+        
+        }
+        addPeer(peer) {
+            this.peers.set(peer.remotePeerId, peer);
+        }
+        removePeer(peerId) {
+            this.peers.delete(peerId);
+        }
 
-    }
+        sendMessage(peerId, type, payload) {
+            const peer = this.peers.get(peerId);
+            if (!peer) throw new Error("Peer not found");
+            peer.sendMessage(type, payload);
+        }
+
+        broadcastMessage(type, payload) {
+            for (const peer of this.peers.values()) {
+                peer.sendMessage(type, payload);
+            }
+        }
 }
